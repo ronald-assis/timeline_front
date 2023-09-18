6cf84e6d61e03d0f5ea52d3a95fd01970665a6ec
ronald-assis <ronaldassis71@gmail.com>
2023-09-18 18:33:08 -0300

    feat: stripes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
         <div className="blur-full absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50" />
+        <div className="bg-stripes absolute bottom-0 right-2 top-0 w-2" />
       </div>
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
