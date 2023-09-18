72db8c02f2a3a8172b72dfc329d3198bb75d30b1
ronald-assis <ronaldassis71@gmail.com>
2023-09-18 17:42:21 -0300

    feat: empty memory list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 export default function Home() {
   return (
-    <>
-      <h1 className="m-10 h-10 w-10 rounded">Hello World!</h1>
-      <h1 className="font-alt m-10 h-10 w-10 rounded">Hello World!</h1>
-    </>
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
+    <main className="grid min-h-screen grid-cols-2">
+      // left
+      <div className="flex flex-col items-start justify-between px-28 py-16"></div>
+      // right
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center"></div>
+        <p className="w-[360px] text-center leading-relaxed">
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a className="underline hover:text-gray-50">criar agora!</a>
+        </p>
+      </div>
+    </main>
   )
 }
