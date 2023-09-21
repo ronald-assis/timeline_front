3410e51d9aa42cddaba048cfc6a1c476675e19df
ronald-assis <ronaldassis71@gmail.com>
2023-09-21 13:35:06 -0300

    feat: separate components

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="http://rocketseat.com.br"
+        className="underline hover:text-gray-200"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
