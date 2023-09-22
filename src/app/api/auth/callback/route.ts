aef18f4462fba2161aa436ea25d8e779f038971c
ronald-assis <ronaldassis71@gmail.com>
2023-09-22 16:07:42 -0300

    feat: get token of back-end

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from 'next/server'
+import { api } from '@/lib/api'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+  console.log(token, 'Vai da Certo!')
+}
