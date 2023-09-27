cfc9fbc06ca04980fd40cda08ab87235dbdc278c
ronald-assis <ronaldassis71@gmail.com>
2023-09-27 17:39:16 -0300

    feat: page new memory

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -21,8 +21,30 @@ export default function NewMemory() {
             <Camera className="h-4 w-4" />
             Anexar mídia
           </label>
-          <input type="file" id="midia" className="invisible" />
+
+          <label
+            htmlFor="isPublic"
+            className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+          >
+            <input
+              type="checkbox"
+              name="isPublic"
+              id="isPublic"
+              value="true"
+              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+            />
+            Torna memória publica
+          </label>
         </div>
+
+        <input type="file" id="midia" className="invisible h-0 w-0" />
+
+        <textarea
+          name="content"
+          spellCheck={false}
+          className="resize-nome w-full flex-1 rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />
       </form>
     </div>
   )
