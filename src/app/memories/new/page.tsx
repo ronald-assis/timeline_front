afc7ce4b1e06e78d9c2715633c3e75dad6b6120a
ronald-assis <ronaldassis71@gmail.com>
2023-09-27 11:32:51 -0300

    feat: creating new memory

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -1,3 +1,29 @@
-export default function Memories() {
-  return <h1>NEW</h1>
+import Link from 'next/link'
+import { Camera, ChevronLeft } from 'lucide-react'
+
+export default function NewMemory() {
+  return (
+    <div className="flex flex-1 flex-col gap-4">
+      <Link
+        href="/"
+        className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
+      >
+        <ChevronLeft className="h-4 w-4" />
+        voltar à timeline
+      </Link>
+
+      <form className="flex flex-1 flex-col gap-2">
+        <div className="flex items-center gap-4">
+          <label
+            htmlFor="midia"
+            className="flex cursor-pointer items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
+          >
+            <Camera className="h-4 w-4" />
+            Anexar mídia
+          </label>
+          <input type="file" id="midia" className="invisible" />
+        </div>
+      </form>
+    </div>
+  )
 }
